f270e2dde5f413029d8b5d6e86a6b2c19139b65a
JupiterLikeThePlanet <jupiterbaudot@gmail.com>
2016-01-28 17:23:33 -0800

    Add faker gem and faker content in seeds.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+require 'faker'
+
+10.times do
+  User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "password")
+end
+
+100.times do
+  Contact.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, number: Faker::Number.number(10), user_id: rand(1..11))
+end
+
+1000.times do
+  Message.create(user_id: rand(1..11), contact_id: rand(1..102), message: Faker::Lorem.sentence)
+end
+
+# Resource.create(name: "Yo Mamma", link: "http://api.yomomma.info/", access: "joke['joke']")
+# Resource.create(name: "Chuck Norris", link: "http://api.icndb.com/jokes/random?exclude=Array", access: "joke['value']['joke']")
+
+User.create(first_name: "Jupiter", last_name: "Baudot", email: "", password: "test1234")
+# User.create(first_name: "", last_name: "", email: "", password: "")
+Contact.create(first_name: "Imraan", last_name: "Ismail", number: "", user_id: 11)
