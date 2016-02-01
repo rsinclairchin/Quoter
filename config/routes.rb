20a6c600416206c1ea03f8451bd0748b5a68976f
JupiterLikeThePlanet <jupiterbaudot@gmail.com>
2016-01-31 16:34:41 -0800

    Create view files

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
-  resources :welcome #only: [:index]
+  resources :welcome #,only: [:index]
   resources :users
   resources :messages
   resources :contacts
-
+  resource :sessions, only: [:new, :create, :destroy]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
