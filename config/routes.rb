daa7214bdc14d8e76b5cea74c6669c3f49bdf4fb
JupiterLikeThePlanet <jupiterbaudot@gmail.com>
2016-01-31 16:18:19 -0800

    Add index to welcome views and resource routes in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  resources :welcome #only: [:index]
+  resources :users
+  resources :messages
+  resources :contacts
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
