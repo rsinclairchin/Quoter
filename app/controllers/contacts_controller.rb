28cc3b58864fca06282f944b75c1f15f9ffe865e
JupiterLikeThePlanet <jupiterbaudot@gmail.com>
2016-01-31 17:45:42 -0800

    Fill out contact & application controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,35 @@
 class ContactsController < ApplicationController
+
+def index
+  @contacts = Contact.all
+end
+
+def new
+  @contact = Contact.new
+end
+
+def edit
+  @contact = Contact.find(params[:id])
+end
+
+def create
+  @contact = Contact.new(contact_params)
+end
+
+def show
+  @contact = Contact.find(params[:id])
+end
+
+def update
+    @contact = Contact.find(params[:id])
+    if @contact.update(contact_params)
+      redirect_to contacts
+    else
+      render 'edit'
+    end
+end
+
+def destroy
+  @contact = Contact.find(params[:id]).destroy
+  redirect_to contacts
 end
