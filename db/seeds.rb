7d739e8ab6cf389638c0788336148b10cbda29f8
JupiterLikeThePlanet <jupiterbaudot@gmail.com>
2016-02-08 17:07:11 -0800

    Create a resource seed for quotes, API call in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,23 +7,30 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-require 'faker'
+# require 'faker'
 
-10.times do
-  User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "password")
-end
+# 10.times do
+#   User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "password")
+# end
 
-100.times do
-  Contact.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, number: Faker::Number.number(10), user_id: rand(1..11))
-end
+# 100.times do
+#   Contact.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, number: Faker::Number.number(10), user_id: rand(1..11))
+# end
 
-1000.times do
-  Message.create(user_id: rand(1..11), contact_id: rand(1..102), message: Faker::Lorem.sentence)
-end
+# 1000.times do
+#   Message.create(user_id: rand(1..11), contact_id: rand(1..102), message: Faker::Lorem.sentence)
+# end
 
-# Resource.create(name: "Yo Mamma", link: "http://api.yomomma.info/", access: "joke['joke']")
-# Resource.create(name: "Chuck Norris", link: "http://api.icndb.com/jokes/random?exclude=Array", access: "joke['value']['joke']")
+# User.create(first_name: "Jupiter", last_name: "Baudot", email: "", password: "test1234")
+# # User.create(first_name: "", last_name: "", email: "", password: "")
+# Contact.create(first_name: "Imraan", last_name: "Ismail", number: "", user_id: 11)
 
-User.create(first_name: "Jupiter", last_name: "Baudot", email: "", password: "test1234")
-# User.create(first_name: "", last_name: "", email: "", password: "")
-Contact.create(first_name: "Imraan", last_name: "Ismail", number: "", user_id: 11)
+##################################################
+# Resource.create(name: "Quote of the Day", link: "http://quotes.rest/qod.json", access: "quote['content'][quotes][quote]")
+#, "author['content'][quotes][author]"
+#################################################
+
+ User.create(first_name: "Shelly", last_name: "Cat", email: "mail@gmail.com", password: "test1234")
+Contact.create(first_name: "S", last_name: "A", number: "6472029000", user_id: 12)
+
+Contact.create(first_name: "J", last_name: "B", number: "9857885503", user_id: 13)
