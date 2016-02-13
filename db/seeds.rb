e0295cd24e98014440d3711468cd0da617a8c43d
JupiterLikeThePlanet <jupiterbaudot@gmail.com>
2016-02-12 18:36:24 -0800

    Debug message controller to parse Quote data (in progress)

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,26 +13,26 @@
   User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "password")
 end
 
-25.times do
+5.times do
   Contact.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, number: Faker::Number.number(10), user_id: rand(1..11))
 end
 
-10.times do
-  Message.create(user_id: rand(1..11), contact_id: rand(1..102), message: Faker::Lorem.sentence)
-end
- User.create(first_name: "Shelly", last_name: "Cat", email: "mail@gmail.com", password: "test1234")
-Contact.create(first_name: "S", last_name: "A", number: "", user_id: 12)
+# 10.times do
+#   Message.create(user_id: rand(1..11), contact_id: rand(1..102), message: Faker::Lorem.sentence)
+#end
+#User.create(first_name: "Shelly", last_name: "Cat", email: "mail@gmail.com", password: "test1234")
 
-Contact.create(first_name: "J", last_name: "B", number: "", user_id: 13)
-User.create(first_name: "Jupiter", last_name: "Baudot", email: "", password: "test1234")
+User.create(first_name: "Jupiter", last_name: "Baudot", email: "jupiterthecynic@gmail.com", password: "test1234")
 # User.create(first_name: "", last_name: "", email: "", password: "")
 Contact.create(first_name: "Imraan", last_name: "Ismail", number: "", user_id: 11)
+Contact.create(first_name: "S", last_name: "A", number: "", user_id: 11)
 
+Contact.create(first_name: "J", last_name: "B", number: "", user_id: 11)
 ##################################################
 # Resource.create(name: "Quote of the Day", link: "http://quotes.rest/qod.json", access: "quote['content'][quotes][quote]")
 #, "author['content'][quotes][author]"
 
-Resource.create(name: "Quote of the Day", link: "http://quotes.rest/qod.json", access: "quote[1][0][0]")
+Resource.create(name: "Quote of the Day", link: "http://quotes.rest/qod.json", access: "quote[:contents][:quotes][0][:quote]")
 # quote['content'][quotes][quote]
 #, "author['content'][quotes][author]"
 #################################################
