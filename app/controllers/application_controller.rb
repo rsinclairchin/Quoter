3ba7b93da7daed5ab6b374e2763497417c4ab856
JupiterLikeThePlanet <jupiterbaudot@gmail.com>
2016-02-13 20:51:28 -0800

    Re-Enable Pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  #include Pundit
+
   protect_from_forgery with: :exception
   include SessionsHelper
+  include Pundit
 
   # def current_user
   #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
